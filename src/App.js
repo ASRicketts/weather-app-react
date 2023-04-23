43a1e82b70d205a6cdc87228bfd7dbe5d883dd0d
ASRicketts <andrae,ricketts@gmail.com>
2023-04-23 08:11:54 -0400

    React Weather App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React {useState} from 'react';
+import axios from 'axios';
+
+
+function App() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q={Trenton}&appid={227147dbe5614a6fcd5845bd7d406e79}`
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
